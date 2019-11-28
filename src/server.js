304a892568ce224c6d496e2a26f2640594615378
cristianldesouza <cristianldesouza@hotmail.com>
2019-11-28 09:50:45 -0300

    first commit

---
+++ src/server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const db = require('./database');
+const app = express();
+
+
+// Middlewares
+
+
+// Routes
+app.get('/login', async function (req, res) {
+
+});
